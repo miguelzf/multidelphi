86b22c207265876f735ede09462274f4ff5933ec
Relfos <relfos@gmail.com>
2014-02-22 20:10:43 +0000

    Added parser sample

--- src/main.cs
+++ src/main.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using DGrok.DelphiNodes;
 using DGrok.Framework;
@@ -14,11 +15,23 @@ namespace crosspascal
         static void Main(string[] args)
         {
             RuleType _ruleType = RuleType.Goal;
-            string text = "d:/code/leaf3_terra/trunk/utils";
-            Parser parser = Parser.FromText(text, "input", CompilerDefines.CreateStandard(), new MemoryFileLoader());
-            AstNode tree = parser.ParseRule(_ruleType);
-            Console.WriteLine(tree.Inspect());
+            try
+            {
+                string fileName = "d:/code/crosspascal/tests/test_record_with_methods.dpr";
+                string text = File.ReadAllText(fileName);
+                Parser parser = Parser.FromText(text, fileName, CompilerDefines.CreateStandard(), new MemoryFileLoader());
+                AstNode tree = parser.ParseRule(_ruleType);
+                Console.WriteLine(tree.Inspect());
+            }
+            catch (DGrokException ex)
+            {
+                string error = "Filename: " + ex.Location.FileName + Environment.NewLine +
+                    "Offset: " + ex.Location.Offset + Environment.NewLine +
+                    ex.Message;
+                Console.WriteLine(error);
+            }
             Console.ReadLine();
+
         }
     }
 }
