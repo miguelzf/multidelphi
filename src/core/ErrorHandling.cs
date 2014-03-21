155c8db1149d1265404999b9d107106196e94e6d
miguelzf <eligmu@gmail.com>
2014-03-21 19:50:25 +0000

    Added Error exceptions

--- src/core/ErrorHandling.cs
+++ src/core/ErrorHandling.cs
@@ -16,6 +16,11 @@ namespace crosspascal.core
 			: base (message) { }
 	}
 
+
+	//
+	// Internal Errors
+	//
+
 	public class AstNodeException : CrossPascalException
 	{
 		const string DefaultMsg = "Error creating an AST Node";
@@ -23,6 +28,24 @@ namespace crosspascal.core
 		public AstNodeException (int lineno, string message = DefaultMsg) : base (lineno,message) { }
 		
 		public AstNodeException (string message = DefaultMsg) : base (message) { }
+	}
+
+	public class NotImplementedException : CrossPascalException
+	{
+		const string DefaultMsg = " not implemented";
+
+		public NotImplementedException(int lineno, string message) : base(lineno, message + DefaultMsg) { }
+
+		public NotImplementedException(string message) : base(message + DefaultMsg) { }
+	}
+
+	public class InvalidAbstractException : CrossPascalException
+	{
+		const string DefaultMsg = " method should never be called";
+
+		public InvalidAbstractException(int lineno, string message) : base(lineno, message + DefaultMsg) { }
+
+		public InvalidAbstractException(string message) : base(message + DefaultMsg) { }
 	}
 
 
